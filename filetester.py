043db66bf93372e8b5ae040df106e3fca52d259b
erikgust2 <erikgust2@gmail.com>
2023-01-27 21:11:10 +0100

    Improved annotations in filetester.py

--- filetester.py
+++ filetester.py
@@ -1,11 +1,13 @@
 import openai
 
+# Gets OpenAI API key from api.txt
+# NOTE: YOU HAVE TO MANUALLY ADD THIS TEXTFILE WITH YOUR OWN KEY
 f = open('apikey.txt')
 API_KEY = f.read()
 f.close()
-
 openai.api_key = API_KEY
 
+# Enter the language model you wish to use. Default is the latest and most complicated model - DaVinci
 model = 'text-davinci-003'
 
 # User input of the query as well as attaching the file containing the code
