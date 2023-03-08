a406452086018fa6288f1f3a3cceac4403438017
Erik Gustafsson <erikgust2@gmail.com>
2023-03-08 16:30:17 +0100

    Added code comments to filetester_automatic.py

--- filetester_automatic.py
+++ filetester_automatic.py
@@ -1,6 +1,7 @@
 import os
 import openai
 
+# Name of the output file. Change this to whatever you want.
 outputFileName = 'output.txt'
 
 # Gets OpenAI API key from api.txt
@@ -16,22 +17,30 @@ model = 'text-davinci-003'
 # Edit these to change the categories of the query
 categories = ['BMI', 'Fahrenheit', 'Sorting']
 
+# Creates the output file
 outputFile = open(outputFileName, 'w')
 
-# Iterates through each category
+# Prompts OPENAI to generate a response for each file in each category
 for category in categories:
 
+    # Writes the category name to the output file to signify a new category
     outputFile.write('Category: ' + category + '\n\n')
 
+    # Writes the description of the prompt to the output file
     descriptionFile = open('dataset/' + category + '/' + category + '_description.txt')
     description = descriptionFile.read() + '\n\n'
     descriptionFile.close()
 
+    # Iterates through each file in the category
     for file in os.listdir('dataset/' + category):
 
+        # Ignores the description file
         if file.endswith('.py'):
+            # Writes the file name to the output file to signify a new file
             outputFile.write(file + '\n\n')
             f = open('dataset/' + category + '/' + file)
+
+            # Generates the prompt
             prompt = description + f.read()
             f.close()
 
@@ -43,9 +52,8 @@ for category in categories:
                 temperature=0.7,
                 n=1
             )
-            
-            # outputFile.write(prompt + '\n\n' + '------------------------' + '\n\n')
 
+            # Writes the response to the output file
             for choice in response.choices:
                 outputFile.write(choice.text + '\n\n' + '------------------------' + '\n\n')
 
